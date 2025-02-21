7671573826c919d9f00de7341042a9c83d9d89b4
shubh <shubh@ABC>
2025-02-21 19:03:31 +0530

    Change in My first step dep file under FirstFeatureFile package.

--- CucAuto/src/test/java/com/basic/FirstFeatureFile/MyFirstStepDef.java
+++ CucAuto/src/test/java/com/basic/FirstFeatureFile/MyFirstStepDef.java
@@ -22,6 +22,7 @@ public class MyFirstStepDef
 			Thread.sleep(3000);
 			driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 			driver.manage().window().maximize();
+			System.out.println("Window maximize successfully.");
 		}
 		catch(Exception e)
 		{
